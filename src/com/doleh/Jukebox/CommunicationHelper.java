065ff28ed460280c8b67f3212fffde5fbff5825e
Mohammad Doleh <mdoleh@kent.edu>
2013-10-20 04:52:42 -0400

    Revert "WiFi direct setup"
    This reverts commit 6858de2f6d1f9d2b2cf3c6d159bca3b1d37792ab.

---
+++ src/com/doleh/Jukebox/CommunicationHelper.java
@@ -0,0 +1,57 @@
+package com.doleh.Jukebox;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.IntentFilter;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.os.Bundle;
+
+class CommunicationHelper extends Activity
+{
+    private IntentFilter intentFilter = new IntentFilter();
+    private WifiP2pManager.Channel mChannel;
+    private WifiP2pManager mManager;
+    private BroadcastReceiver receiver;
+    private MainActivity activity;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        //  Indicates a change in the Wi-Fi P2P status.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+
+        // Indicates a change in the list of available peers.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+
+        // Indicates the state of Wi-Fi P2P connectivity has changed.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+
+        // Indicates this device's details have changed.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+
+        mManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        mChannel = mManager.initialize(this, getMainLooper(), null);
+    }
+
+    /** register the BroadcastReceiver with the intent values to be matched */
+    @Override
+    public void onResume() {
+        super.onResume();
+        receiver = new WiFiDirectBroadcastReceiver(mManager, mChannel, activity);
+        registerReceiver(receiver, intentFilter);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        unregisterReceiver(receiver);
+    }
+
+    public void setActivity(MainActivity mActivity)
+    {
+        activity = mActivity;
+    }
+}
--- src/com/doleh/Jukebox/MainActivity.java
+++ src/com/doleh/Jukebox/MainActivity.java
@@ -3,32 +3,21 @@ package com.doleh.Jukebox;
 import android.app.ActionBar;
 import android.app.Activity;
 import android.app.FragmentTransaction;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
 import android.media.MediaPlayer;
-import android.net.wifi.p2p.WifiP2pDevice;
-import android.net.wifi.p2p.WifiP2pManager;
-import android.net.wifi.p2p.WifiP2pDeviceList;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.*;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static java.sql.DriverManager.println;
-
 public class MainActivity extends Activity
 {
-    private MediaLibraryHelper mediaLibraryHelper;
-    private IntentFilter intentFilter = new IntentFilter();
-    private WifiP2pManager.Channel mChannel;
-    private WifiP2pManager mManager;
-    private BroadcastReceiver receiver;
-    private List peers = new ArrayList();
-
+    MediaLibraryHelper mediaLibraryHelper;
+    CommunicationHelper communicationHelper;
     /**
      * Called when the activity is first created.
      */
@@ -40,67 +29,6 @@ public class MainActivity extends Activity
 
         createTabs();
         setupButtonEventListener();
-
-        //  Indicates a change in the Wi-Fi P2P status.
-        intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
-
-        // Indicates a change in the list of available peers.
-        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
-
-        // Indicates the state of Wi-Fi P2P connectivity has changed.
-        intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
-
-        // Indicates this device's details have changed.
-        intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
-
-        mManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
-        mChannel = mManager.initialize(this, getMainLooper(), null);
-
-        mManager.discoverPeers(mChannel, new WifiP2pManager.ActionListener() {
-
-            @Override
-            public void onSuccess() {
-                // Code for when the discovery initiation is successful goes here.
-                // No services have actually been discovered yet, so this method
-                // can often be left blank.  Code for peer discovery goes in the
-                // onReceive method, detailed below.
-            }
-
-            @Override
-            public void onFailure(int reasonCode) {
-                // Code for when the discovery initiation fails goes here.
-                // Alert the user that something went wrong.
-            }
-        });
-
-        WifiP2pManager.PeerListListener peerListListener = new WifiP2pManager.PeerListListener() {
-            @Override
-            public void onPeersAvailable(WifiP2pDeviceList peerList) {
-
-                // Out with the old, in with the new.
-                peers.clear();
-                peers.addAll(peerList.getDeviceList());
-
-                if (peers.size() == 0) {
-                    println("No devices found");
-                    return;
-                }
-            }
-        };
-    }
-
-    /** register the BroadcastReceiver with the intent values to be matched */
-    @Override
-    public void onResume() {
-        super.onResume();
-        receiver = new WiFiDirectBroadcastReceiver(mManager, mChannel, this);
-        registerReceiver(receiver, intentFilter);
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        unregisterReceiver(receiver);
     }
 
     private void createTabs()
@@ -232,6 +160,8 @@ public class MainActivity extends Activity
         final EditText songArtist = (EditText)findViewById(R.id.songArtist);
 
         // Start listening for song requests
+        communicationHelper = new CommunicationHelper();
+        communicationHelper.setActivity(this);
 
 
         // Check if requested song exists
@@ -249,60 +179,5 @@ public class MainActivity extends Activity
             System.out.println("No matching songs found.");
         }
     }
-
-    private class WiFiDirectBroadcastReceiver extends BroadcastReceiver
-    {
-        private WifiP2pManager manager;
-        private WifiP2pManager.Channel channel;
-        private MainActivity activity;
-
-        public WiFiDirectBroadcastReceiver(WifiP2pManager manager, WifiP2pManager.Channel channel,
-                                           MainActivity activity) {
-            super();
-            this.manager = manager;
-            this.channel = channel;
-            this.activity = activity;
-        }
-
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
-            if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) {
-                // Determine if Wifi P2P mode is enabled or not, alert
-                // the Activity.
-                int state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1);
-                if (state == WifiP2pManager.WIFI_P2P_STATE_ENABLED) {
-                    // do stuff
-                } else {
-                    // do stuff
-                }
-            } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
-
-                // The peer list has changed!  We should probably do something about
-                // that.
-
-            } else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
-
-                // Connection state changed!  We should probably do something about
-                // that.
-
-            } else if (WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)) {
-//            DeviceListFragment fragment = (DeviceListFragment) activity.getFragmentManager()
-//                    .findFragmentById(R.id.frag_list);
-//            fragment.updateThisDevice((WifiP2pDevice) intent.getParcelableExtra(
-//                    WifiP2pManager.EXTRA_WIFI_P2P_DEVICE));
-            }
-        }
-    }
-
-    private class WiFiPeerListAdapter extends ArrayAdapter<WifiP2pDevice>
-    {
-        private List<WifiP2pDevice> items;
-
-        public WiFiPeerListAdapter(Context context, int textViewResourceId,
-                                   List<WifiP2pDevice> objects) {
-            super(context, textViewResourceId, objects);
-            items = objects;
-        }
-    }
 }
+
