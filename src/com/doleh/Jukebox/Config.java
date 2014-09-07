b4da17f6a69ee38fbbb0d8a2554333c351689c5a
Mohammad Doleh <mdoleh@kent.edu>
2014-09-07 11:43:34 -0400

    Configurable max message count

--- src/com/doleh/Jukebox/Config.java
+++ src/com/doleh/Jukebox/Config.java
@@ -7,7 +7,7 @@ public class Config
     public static String APP_PNAME;
     public static String APP_TITLE;
     public static boolean SHOULD_SHOW_ADS;
-    public static final int MAX_MESSAGE_COUNT = 3;
+    public static int MAX_MESSAGE_COUNT;
     /** networking port that server listens on */
     public static final int PORT = 35768;
 
@@ -15,11 +15,13 @@ public class Config
     public static final String SHOULD_SHOW_ADS_KEY = "com.doleh.Jukebox.MainActivity.show_ads";
     public static final String APP_PNAME_KEY = "com.doleh.Jukebox.MainActivity.app_pname";
     public static final String APP_TITLE_KEY = "com.doleh.Jukebox.MainActivity.app_title";
+    public static final String MAX_MESSAGE_COUNT_KEY = "com.doleh.Jukebox.MainActivity.max_message_count";
 
     public static void initialize(Intent intent)
     {
         APP_PNAME = intent.getStringExtra(APP_PNAME_KEY);
         APP_TITLE = intent.getStringExtra(APP_TITLE_KEY);
         SHOULD_SHOW_ADS = intent.getBooleanExtra(SHOULD_SHOW_ADS_KEY, true);
+        MAX_MESSAGE_COUNT = intent.getIntExtra(MAX_MESSAGE_COUNT_KEY, 3);
     }
 }
