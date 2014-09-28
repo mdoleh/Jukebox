c6bd0cfde2a1ec4348e6e0e7f476fd82029dff5c
Mohammad Doleh <mdoleh@kent.edu>
2014-09-28 18:37:46 -0400

    ConfigUpdated message object created

---
+++ src/com/doleh/Jukebox/MessageTypes/Server/ConfigUpdated.java
@@ -0,0 +1,23 @@
+package com.doleh.Jukebox.MessageTypes.Server;
+
+import com.doleh.Jukebox.MessageDialog;
+import com.doleh.Jukebox.NetworkClient;
+import com.doleh.Jukebox.R;
+
+import java.io.Serializable;
+
+public class ConfigUpdated extends ServerMessage implements Serializable
+{
+    private int _requestsRemaining;
+
+    public ConfigUpdated(int requestsRemaining)
+    {
+        _requestsRemaining = requestsRemaining;
+    }
+
+    @Override
+    public void Execute()
+    {
+        MessageDialog.showMessageBox(NetworkClient.mainActivity, NetworkClient.mainActivity.getString(R.string.configUpdated), String.format(NetworkClient.mainActivity.getString(R.string.configUpdatedMsg), _requestsRemaining));
+    }
+}
